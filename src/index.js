62540f64e73597f84977dd54bb3431766575cc53
unknown <kibrish49@gmail.com>
2022-11-02 12:08:24 +0200

    Add index.html and the drag features for to do tasks

--- src/index.js
+++ src/index.js
@@ -1,60 +1,50 @@
-import _, { after } from 'lodash';
+import _ from 'lodash';
 import './style.css';
 import * as el from './modules/elementSelector.js';
-import generateTaskList from './modules/generateTaskList'
+import generateTaskList from './modules/generateTaskList.js';
 
 // import { draggables } from './modules/elementSelector';
-
-
-el.draggables.forEach(draggable =>{
-    draggable.addEventListener('dragstart',()=>{
-        draggable.classList.add('dragging');
-        console.log("drag start")
-        
-    })
-    
-    draggable.addEventListener('dragend',()=>{
-        draggable.classList.remove('dragging');
-    })
-});
-
-
-
-el.container.addEventListener('dragover',(e) => {
-  e.preventDefault()
-  const afterElement =getDragAfterElement(el.container,e.clientY); 
-  const draggable= document.querySelector('.dragging');
-  console.log(afterElement);
-  if(afterElement==null){
-    el.container.appendChild(draggable);
-  }else{
-    el.container.insertBefore(draggable,afterElement)
-  }
- 
-})
-
-function getDragAfterElement(container,y){
+function getDragAfterElement(container, y) {
   const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')];
 
-  return draggableElements.reduce((closest,child)=>{
-    const box= child.getBoundingClientRect()
-    const offset= y - box.top - box.height/2;
-    if(offset<0 && offset>closest.offset){
-      return {offset: offset, element:child}
+  return draggableElements.reduce((closest, child) => {
+    const box = child.getBoundingClientRect();
+    const offset = y - box.top - box.height / 2;
+    if (offset < 0 && offset > closest.offset) {
+      return { offset, element: child };
     }
-    else{
-      return closest;
-    }
-  },{offset:Number.NEGATIVE_INFINITY}).element;
+
+    return closest;
+  }, { offset: Number.NEGATIVE_INFINITY }).element;
 }
+
+el.draggables.forEach((draggable) => {
+  draggable.addEventListener('dragstart', () => {
+    draggable.classList.add('dragging');
+  });
+
+  draggable.addEventListener('dragend', () => {
+    draggable.classList.remove('dragging');
+  });
+});
+
+el.container.addEventListener('dragover', (e) => {
+  e.preventDefault();
+  const afterElement = getDragAfterElement(el.container, e.clientY);
+  const draggable = document.querySelector('.dragging');
+  if (afterElement == null) {
+    el.container.appendChild(draggable);
+  } else {
+    el.container.insertBefore(draggable, afterElement);
+  }
+});
 
 function component() {
   const element = document.createElement('div');
 
-  // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  // element.classList.add('hello');
-  generateTaskList()
-  
+  element.innerHTML = _.join(['Welcome', 'to your daily task!'], ' ');
+  generateTaskList();
+
   return element;
 }
 
--- src/modules/dragElement.js
+++ src/modules/dragElement.js
@@ -1,13 +1,11 @@
 import * as el from './elementSelector.js';
 
-el.draggables.forEach(draggable =>{
-    draggable.addEventListener('dragstart',()=>{
-        draggable.classList.add('dragging');
-        console.log("drag")
-    })
-    
-    draggable.addEventListener('dragend',()=>{
-        draggable.classList.remove('dragging');
-    })
+el.draggables.forEach((draggable) => {
+  draggable.addEventListener('dragstart', () => {
+    draggable.classList.add('dragging');
+  });
+
+  draggable.addEventListener('dragend', () => {
+    draggable.classList.remove('dragging');
+  });
 });
-console.log("first")
